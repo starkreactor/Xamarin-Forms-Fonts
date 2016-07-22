defab8a3850704af06f0e19a9cdb7a44b172724c
Michael Stark <mws580@hotmail.com>
2016-07-22 08:41:39 -0400

    First commit

---
+++ FontIcons/Components/IconFontLabel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FontIcons
+{
+    /// <summary>
+    /// This call allows the rendering of icon fonts held within the cc-fonts-4.ttf file on each of the platform projects. Use the IconFont property to set which icon should be used
+    /// </summary>
+    public class IconFontLabel : Label
+    {
+
+        public IconFontLabel ()
+        {
+            //We need to load the font family for iOS by specifying it here
+            FontFamily = "FontAwesome";
+
+            //For android, we set it in the custom rendered in the Droid project under the IconFontLabelRenderer
+        }
+
+        public static readonly BindableProperty IconFontProperty = BindableProperty.Create<IconFontLabel, Object> (
+                                                                           p => p.IconFont,
+                                                                       null,
+                                                                       propertyChanged: (bindable,
+                                                                                         oldValue,
+                                                                                         newValue) => ((IconFontLabel)bindable).SetIconFontText (newValue)
+                                                                   );
+
+        /// <summary>
+        /// This is the UTF-8 integer or hex representation of the character in the cc-fonts-4.ttf file in the assets folder for Android or the resources folder for iOS e.g. 0xE029 or E029
+        /// </summary>
+        public Object IconFont {
+            get { return (Object)GetValue (IconFontProperty); }
+            set { SetValue (IconFontProperty, value); }
+        }
+
+        public void SetIconFontText (object o)
+        {
+            if (o is String) {
+                var utf8_ref = Int32.Parse ((String)o, System.Globalization.NumberStyles.HexNumber);
+                Text = Char.ConvertFromUtf32 (utf8_ref);
+            }
+
+            if (o is Int32) {
+                Text = Char.ConvertFromUtf32 ((Int32)o);
+            }
+        }
+    }
+}
+
---
+++ FontIcons/FontIconsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace FontIcons
+{
+	public partial class FontIconsPage : ContentPage
+	{
+		public FontIconsPage()
+		{
+			InitializeComponent();
+
+
+		}
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			var panel = new StackLayout();
+			var btn = new IconFontButton() { IconFont = "F000", FontSize=30 };
+			var btn1 = new IconFontButton() { IconFont = "F013", FontSize=30 };
+			var btn2 = new IconFontButton() { IconFont = "F06E",FontSize=30 };
+			var btn3 = new IconFontButton() { IconFont = "F087",FontSize=60 };
+			MainGrid.Children.Add(btn, 1, 1);
+			MainGrid.Children.Add(btn1, 1, 2);
+			MainGrid.Children.Add(btn2, 3, 1);
+			MainGrid.Children.Add(btn3, 3, 2);
+		}
+
+	}
+}
+
